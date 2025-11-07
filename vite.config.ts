56e629179317f8e8aa4fdeda02d125841435308e
Oleg <oleg.verhoglyad@gmail.com>
2025-11-07 17:56:37 +0200

    Mock folder was changed.

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,3 @@
-// import { defineConfig, loadEnv, searchForWorkspaceRoot } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-// import fs from 'node:fs'
-
-// // https://vite.dev/config/
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd());
-
-//   return {
-//     plugins: [react(), tailwindcss(),
-//       {
-//         name: 'run-script',
-//         enforce: 'pre',
-//         writeBundle: () => fs.copyFile(
-//           `${__dirname}/assets/app.config.json`,
-//           `${__dirname}/dist/WebResources/${env.VITE_CRM_SOLUTION_PREFIX}/advanced-find/app.config.json`,
-//           (error: any) => error ?? console.error(error)
-//         )
-//       }
-//     ],
-//     build: {
-//       rollupOptions: {
-//         output: {
-//           entryFileNames: `WebResources/${env.VITE_CRM_SOLUTION_PREFIX}/advanced-find/[name].js`,
-//           chunkFileNames: `WebResources/${env.VITE_CRM_SOLUTION_PREFIX}/advanced-find/[name].js`,
-//           assetFileNames: `WebResources/${env.VITE_CRM_SOLUTION_PREFIX}/advanced-find/[name].[ext]`
-//         }
-//       }
-//     },
-//     server: {
-//       fs: {
-//         allow: [searchForWorkspaceRoot(process.cwd()), 'assets', 'design-data']
-//       }
-//     }
-//   }
-// })
-
-
 import path from 'node:path'
 import fs from 'node:fs'
 import react from '@vitejs/plugin-react-swc'
@@ -87,7 +48,7 @@ export default defineConfig(({ mode }) => {
     server: {
       host: '0.0.0.0',
       fs: {
-        allow: [searchForWorkspaceRoot(process.cwd()), 'assets', 'design-data']
+        allow: [searchForWorkspaceRoot(process.cwd()), 'assets', 'mock-data']
       }
     }
   };
